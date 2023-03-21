5cf2a8b4ea7457b638974133838c9aaafe9c1c91
Jahid Quraishi <jahid.quraishi@nice.com>
2023-03-21 23:33:05 +0530

    "updated main.java"

--- src/com/pkg/organised/Main.java
+++ src/com/pkg/organised/Main.java
@@ -3,6 +3,12 @@ package com.pkg.organised;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi there");
+        loveYouFun();
+    }
+
+    private static void loveYouFun() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("I love you Amina");
+        }
     }
 }
